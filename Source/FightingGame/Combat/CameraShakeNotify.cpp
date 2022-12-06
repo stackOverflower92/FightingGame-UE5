cb4621b7b98b710621d7a6c1eaf60017ee624416
Giammarco Agazzotti <stackOverflower92@users.noreply.github.com>
2022-12-06 18:22:57 +0100

    Add camera shake when hitting ground

---
+++ Source/FightingGame/Combat/CameraShakeNotify.cpp
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Giammarco Agazzotti
+
+
+#include "CameraShakeNotify.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void UCameraShakeNotify::Notify( USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference )
+{
+	Super::Notify( MeshComp, Animation, EventReference );
+
+	if( auto* PC = UGameplayStatics::GetPlayerController( MeshComp->GetWorld(), 0 ) )
+	{
+		PC->PlayerCameraManager->StartCameraShake( m_Shake );
+	}
+}
---
+++ Source/FightingGame/Combat/CameraShakeNotify.h
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Giammarco Agazzotti
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CameraShakeNotify.generated.h"
+
+UCLASS()
+class FIGHTINGGAME_API UCameraShakeNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify( USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference ) override;
+
+protected:
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, DisplayName = "Shake" )
+	TSubclassOf<UCameraShakeBase> m_Shake = nullptr;
+};
